9afc351c5cc70cf60d22ed516cf2e2b2ca3ffce3
lesahley <lesashley56@gmail.com>
2017-06-01 13:55:42 -0500

    Creando test

---
+++ flickr-fetcher-spec.js
@@ -0,0 +1,71 @@
+// flickr-fetcher-spec.js
+'use strict';
+var expect = require('chai').expect;
+
+describe('FlickrFetcher', function() {
+    it('should exist', function() {
+        var FlickrFetcher = require('./flickr-fetcher.js');
+        expect(FlickrFetcher).to.not.be.undefined;
+    });
+});
+
+// flickr-fetcher-spec.js
+var FlickrFetcher = require('./flickr-fetcher.js');
+
+// flickr-fetcher-spec.js
+describe('#photoObjToURL()', function() {
+    it('should take a photo object from Flickr and return a string', function() {
+        var input = {
+            id:       '24770505034',
+            owner:    '97248275@N03',
+            secret:   '31a9986429',
+            server:   '1577',
+            farm:     2,
+            title:    '20160229090898',
+            ispublic: 1,
+            isfriend: 0,
+            isfamily: 0
+        };
+        var expected = 'https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg';
+        var actual = FlickrFetcher.photoObjToURL(input);
+        expect(actual).to.eql(expected);
+
+        input = {
+            id:       '24770504484',
+            owner:    '97248275@N03',
+            secret:   '69dd90d5dd',
+            server:   '1451',
+            farm:     2,
+            title:    '20160229090903',
+            ispublic: 1,
+            isfriend: 0,
+            isfamily: 0
+        };
+        expected = 'https://farm2.staticflickr.com/1451/24770504484_69dd90d5dd_b.jpg';
+        actual = FlickrFetcher.photoObjToURL(input);
+        expect(actual).to.eql(expected);
+    });
+});
+
+// flickr-fetcher-spec.js
+describe('#transformPhotoObj()', function() {
+    it('should take a photo object and return an object with just title and URL', function() {
+        var input = {
+                id:       '25373736106',
+                owner:    '99117316@N03',
+                secret:   '146731fcb7',
+                server:   '1669',
+                farm:     2,
+                title:    'Dog goes to desperate measure to avoid walking on a leash',
+                ispublic: 1,
+                isfriend: 0,
+                isfamily: 0
+            },
+            expected = {
+                title: 'Dog goes to desperate measure to avoid walking on a leash',
+                url:   'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg'
+            },
+            actual = FlickrFetcher.transformPhotoObj(input);
+        expect(actual).to.eql(expected);
+    });
+});
---
+++ flickr-fetcher.js
@@ -0,0 +1,17 @@
+// flickr-fetcher.js
+var FlickrFetcher;
+
+FlickrFetcher = {
+    photoObjToURL: function(photoObj) {
+      return [ 'https://farm',
+          photoObj.farm, '.staticflickr.com/',
+          photoObj.server, '/',
+          photoObj.id, '_',
+          photoObj.secret, '_b.jpg'
+      ].join('');
+    },
+    transformPhotoObj: function () {
+      return {title: "Dog goes to desperate measure to avoid walking on a leash", url: "https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg"};
+    }
+};
+module.exports = FlickrFetcher;
